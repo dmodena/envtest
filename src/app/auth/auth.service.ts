e92212130cbd0f2789e4e4a12b3b952c94760185
Douglas Modena <d.modena@live.com>
2020-06-20 14:01:46 -0300

    using env variables

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { IUserLoginDto } from '../dto/user-login-dto';
 import { IUserTokenDto } from '../dto/user-token-dto';
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private _loginUrl = "https://localhost:5001/api/v1/auth/login"
+  private _loginUrl = `${environment.api.domain}/api/v1/auth/login`
   private userTokenDto = { } as IUserTokenDto
 
   constructor(private http: HttpClient) { }
